467a0a5244d3c05809c6fba8d3b030cfb4f60741
huyduc <huynguyen26nt@gmail.com>
2024-09-11 04:25:31 +0700

    upd

---
+++ template.cpp
@@ -0,0 +1,27 @@
+# include <bits/stdc++.h>
+# define fi first
+# define se second
+# define all(a) a.begin() , a.end()
+# define ms(a , n) memset(a , n , sizeof(a))
+# define sz(a) a.size()
+# define endl "\n"
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+typedef pair <ll , ll> pi;
+typedef vector <pi> vii;
+const ll mod = 1e9 +7;
+const ll base = 31;
+const ll maxn = 1e6 + 6;
+inline ll gcd(ll a , ll b){ll r; while(b){r = a % b ; a = b ; b = r;} return a;} 
+inline ll lcm(ll a, ll b){return a / gcd(a , b)*b;}
+
+
+int main()
+{
+    ios_base :: sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+}
